ada20113701aec767e5e848c97f5f91403c6aa9f
aarongaliano <agj32@alu.ua.es>
2025-04-29 09:10:41 +0200

    launch_paper_experiments

--- launch_paper_experiments.sh
+++ launch_paper_experiments.sh
@@ -27,29 +27,29 @@ sub_groups="5 3 1"
 # mv train-$method.$src_lang-clean train-$method.$src_lang; mv train-$method.$tgt_lang-clean train-$method.$src_lang
 # ./train-student.sh $src_lang $student_dir train-$method $dev $test $num_updates
 
-for method in $methods; do    
+for method in $methods; do
     # Translate the monolingual corpora
-    ##python3 translate-pipeline.py $model $monolingual $src_lang $tgt_lang $method $translations > teacher_outputs/$src_lang-$tgt_lang-$method
+    python3 translate-pipeline.py $model $monolingual $src_lang $tgt_lang $method $translations > teacher_outputs/$src_lang-$tgt_lang-$method
 
     # Prepare training data
     corpora=student_$src_lang-$tgt_lang-$method
-    ##mkdir $corpora
-    ##./repeat_sentences.sh $monolingual $corpora/train_$translations.$src_lang $translations # Source corpus
-    ##cp teacher_outputs/$src_lang-$tgt_lang-$method $corpora/train_$translations.$tgt_lang # Target corpus
+    mkdir $corpora
+    ./repeat_sentences.sh $monolingual $corpora/train_$translations.$src_lang $translations # Source corpus
+    cp teacher_outputs/$src_lang-$tgt_lang-$method $corpora/train_$translations.$tgt_lang # Target corpus
 
-    ##for group in $sub_groups; do
-    ##    paste $corpora/train_$translations.$src_lang $corpora/train_$translations.$tgt_lang | python3 get_subgroups.py --groups $group --output-file $corpora/train_$group.$src_lang-$tgt_lang
-    ##    cut -f1 $corpora/train_$group.$src_lang-$tgt_lang > $corpora/train_$group.$src_lang
-    ##    cut -f2 $corpora/train_$group.$src_lang-$tgt_lang > $corpora/train_$group.$tgt_lang
-    ##    rm $corpora/train_$group.$src_lang-$tgt_lang
-    ##done
+    for group in $sub_groups; do
+        paste $corpora/train_$translations.$src_lang $corpora/train_$translations.$tgt_lang | python3 get_subgroups.py --groups $group --output-file $corpora/train_$group.$src_lang-$tgt_lang
+        cut -f1 $corpora/train_$group.$src_lang-$tgt_lang > $corpora/train_$group.$src_lang
+        cut -f2 $corpora/train_$group.$src_lang-$tgt_lang > $corpora/train_$group.$tgt_lang
+        rm $corpora/train_$group.$src_lang-$tgt_lang
+    done
 
     for group in $all_groups; do
-        ##python3 skip_empty_lines.py --src $corpora/train_$group.$src_lang --tgt $corpora/train_$group.$tgt_lang
-        ##rm $corpora/train_$group.$src_lang
-        ##rm $corpora/train_$group.$tgt_lang
-        ##mv $corpora/train_$group.$src_lang-clean $corpora/train_$group.$src_lang
-        ##mv $corpora/train_$group.$tgt_lang-clean $corpora/train_$group.$tgt_lang
+        python3 skip_empty_lines.py --src $corpora/train_$group.$src_lang --tgt $corpora/train_$group.$tgt_lang
+        rm $corpora/train_$group.$src_lang
+        rm $corpora/train_$group.$tgt_lang
+        mv $corpora/train_$group.$src_lang-clean $corpora/train_$group.$src_lang
+        mv $corpora/train_$group.$tgt_lang-clean $corpora/train_$group.$tgt_lang
 
         # Launch training
         mkdir -p $corpora/$method-$group
